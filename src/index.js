4b7de81ee4afdb33f5b7407e50e9f3d66d97596b
Neil Wick <neil@wick.net>
2021-04-16 09:52:23 -0400

    Complete, delete, and refresh

--- src/index.js
+++ src/index.js
@@ -8,12 +8,73 @@ import TodoItems from './hbs/todoItems.hbs';
 import Todos from './js/todoApi';
 
 let app = document.getElementById('app');
-app.innerHTML = TodoView();
+app.innerHTML = TodoView({
+	now: new Date().toISOString()
+	});
 
 let todos = new Todos('https://localhost:5001/api/');
 
-todos.getTodos().then((list) => {
-	let itemsList = document.getElementById('items');
-	console.log(list);
-	itemsList.innerHTML = TodoItems(list);
+let init = function() {
+	todos.getTodos().then((list) => {
+		let itemsList = document.getElementById('items');
+		itemsList.innerHTML = "";
+		console.log(list);
+		itemsList.innerHTML = TodoItems(list);
+		addHandlers();
+	});
+};
+
+
+let addHandlers = function() {
+	let completeBtns = document.querySelectorAll("div.task-complete > button");
+	completeBtns.forEach((btn) => {
+		btn.addEventListener("click", async (e) => {
+			let id = e.target.dataset.id;
+	
+			await todos.setComplete(id);
+			init();
+		})
+	});
+
+	let deleteBtns = document.querySelectorAll("div.task-delete > button");
+	deleteBtns.forEach((btn) => {
+		btn.addEventListener("click", async (e) => {
+			let id = e.target.dataset.id;
+
+			await todos.remove(id);
+			init();
+		})
+	});
+
+}
+
+
+document.getElementById("add").addEventListener("click", () => {
+	let elName = document.getElementById("name");
+	let elDeadline = document.getElementById("deadline");
+
+	let data = {
+		"name": elName.value,
+		"deadline": elDeadline.value
+	}
+	todos.addTodo(data);
+
+	elName.value = "";
+	elDeadline.value = new Date().toISOString();
+
+	init();
+	/*
+	{
+    "name": "Wash the car",
+    "isComplete": false,
+    "deadline": "2021-04-30",
+    "responsible": {
+        "PersonId": 4,
+        "name": "Leonard"
+    },
+    "Category": "Transportation"
+}
+	*/
 });
+
+init();
--- src/js/todoApi.js
+++ src/js/todoApi.js
@@ -4,14 +4,67 @@ export default class {
 	}
 
 	async getTodos() {
-		let raw = await fetch(this.baseUrl + 'todoitems/', {
+		let raw = await fetch(this.baseUrl + 'TodoItem/', {
 			mode: 'cors',
 			cache: 'no-cache',
 			method: 'GET',
 		});
-
+		
 		let pdata = await raw.json();
-		//console.log(pdata);
+		console.log(pdata);
 		return pdata;
+	}
+
+	// {
+	// 		"todoItemId": "8",
+	// 		"isComplete": true
+	// }
+
+	async setComplete(id, complete = true) {
+		let payload = {
+			"todoItemId": id,
+			"isComplete": complete
+		};
+
+		let req = await fetch(
+			this.baseUrl + `TodoItem/${id}`,
+			{
+				method: 'PATCH',
+				headers: {
+					"accept": "application/json",
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify(payload)
+			}
+		);
+
+		return;
+	}
+
+	async remove(id) {
+		await fetch(
+			this.baseUrl + `TodoItem/${id}`,
+			{
+				method: 'DELETE'
+			}
+		);
+		return;
+	}
+
+	async addTodo(todo) {
+		let req = await fetch(
+			this.baseUrl + 'TodoItem/', 
+			{
+				method: 'POST',
+				headers: {
+					"accept": "application/json",
+					"Content-Type": "application/json"
+				},
+				body:JSON.stringify(todo)
+			}
+		);
+		let res = await req.json();
+
+		return res;
 	}
 }
